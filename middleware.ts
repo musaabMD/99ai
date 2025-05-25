9f06f7429b55e49256b942f53a429a4e88e9d9dd
musaabMD <mousab.r@outlook.com>
2025-05-25 18:44:20 +0300

    Your commit message

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/profile(.*)',
+])
+
+export default clerkMiddleware((auth, req) => {
+  // Temporarily disabled to debug 500 error
+  // if (isProtectedRoute(req)) auth().protect()
+})
 
 export const config = {
   matcher: [
